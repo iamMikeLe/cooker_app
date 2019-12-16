3f4cadc02bdf742775350e8a8253b0d95a5c35f8
Mike Le <iam@mike-le.info>
2019-12-16 20:15:24 +0100

    installed mui theme

--- src/Pages/examplePage/ExamplePage.jsx
+++ src/Pages/examplePage/ExamplePage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
 const examplePage = () => {
   return (
     <div>
-      <p>Example page</p>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
     </div>
   );
 };
