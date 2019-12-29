d6fb4fbccf2e22ea434c48cc339a55b2b41c9dd1
Mike Le <iam@mike-le.info>
2019-12-29 18:53:26 +0100

    service worker

--- src/serviceWorkerDev.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-export default function registerServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}sw.js`)
-      .then((register) => console.log("worked", register))
-      .catch((err) => console.log("Error", err));
-  }
-}
