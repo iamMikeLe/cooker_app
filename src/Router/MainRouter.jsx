95f0bbea85c65d76b536f8062d52ebc416d2f228
Mike Le <iam@mike-le.info>
2020-01-21 23:22:14 +0100

    remove redundant import

--- src/Router/MainRouter.jsx
+++ src/Router/MainRouter.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // pages
-import ExamplePage from "Pages/examplePage/ExamplePage";
 import Homepage from "Pages/homepage/Homepage";
 
 const MainRouter = () => {
@@ -10,7 +9,6 @@ const MainRouter = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/homepage/" />} />
-        <Route exact path="/examplePage" component={ExamplePage} />
         <Route exact path="/homepage" component={Homepage} />
         <Route path="*" component={() => <p>error page</p>} />
       </Switch>
